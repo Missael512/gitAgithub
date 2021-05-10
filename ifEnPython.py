a6f62dd91749aa714335831b804bfc03febdd296
Missael de la cruz Hernandez <delacruz.hrz.missael@gmail.com>
2021-05-10 15:21:58 -0500

    Cambios en sctipPY

--- ifEnPython.py
+++ ifEnPython.py
@@ -1,8 +1,9 @@
 print("Hola mundo if en python")
-
-a = 18
+print("____ Tratando de hacer un merge sin conflictos ___________")
+a = 19
 if a==18:
     print("ok")
 else:
     print("No ok")
 #Cosas nuevas
+print("Cambiando cosas")
